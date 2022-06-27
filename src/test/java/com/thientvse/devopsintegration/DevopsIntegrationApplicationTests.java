7b6ad4d11314ef60b71d73a819b54ee3f38cb6d3
Thieu Thien <thientv7@viettel.com.vn>
2022-06-27 15:43:44 +0700

    first commit

---
+++ src/test/java/com/thientvse/devopsintegration/DevopsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thientvse.devopsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
