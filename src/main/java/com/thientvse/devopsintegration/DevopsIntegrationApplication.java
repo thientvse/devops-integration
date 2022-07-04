6d7efaa00fcc4d72bac4e02bab74a09b183445ba
Thieu Thien <thientv7@viettel.com.vn>
2022-07-04 14:57:16 +0700

    add spring profiles

--- src/main/java/com/thientvse/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/thientvse/devopsintegration/DevopsIntegrationApplication.java
@@ -1,14 +1,19 @@
 package com.thientvse.devopsintegration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DevopsIntegrationApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(DevopsIntegrationApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsIntegrationApplication.class, args);
         //test
+        LOGGER.info("Springboot profiles application is running successfully.");
     }
 
 }
---
+++ src/main/java/com/thientvse/devopsintegration/configurations/ProfileConfig.java
@@ -0,0 +1,29 @@
+package com.thientvse.devopsintegration.configurations;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Configuration
+public class ProfileConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfileConfig.class);
+
+    // @Profile annotation is used to conditionally activate/register
+    // Used to develop an "if-then-else" conditional checking to configure
+    // Allows to register beans by condition
+    @Profile(value = "dev")
+    @Bean
+    public void devConfig() {
+        LOGGER.info("Successfully loaded the development environment.");
+    }
+
+
+    @Profile(value = "prod")
+    @Bean
+    public void prodConfig() {
+        LOGGER.info("Successfully loaded the production environment.");
+    }
+}
